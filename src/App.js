c082f8695efcb98c561f9f00302e005b3d807877
Khanz1998 <zahrakhan282@gmail.com>
2022-03-07 16:33:28 +0000

    Added Github repo + LinkedIn

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 import "./App.css";
 
 export default function App() {
@@ -5,6 +7,24 @@ export default function App() {
     <div className="App">
       <header className="App">
         <h1>Weather Wise</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/zahrakhan1998/"
+            target="_blank"
+            rel="norefermer"
+          >
+            Zahra Khan
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Khanz1998/react-weather-search-project"
+            target="_blank"
+            rel="norefermer"
+          >
+            open-sourced on Github
+          </a>{" "}
+        </footer>
       </header>
     </div>
   );
